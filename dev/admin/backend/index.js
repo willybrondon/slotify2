b568cab283f375b3a1333f2848b16b8c38e3ff29
willybrondon <ndanguewilly@gmail.com>
2025-07-26 13:54:26 +0200

    fix_version_express_rollback

--- dev/admin/backend/index.js
+++ dev/admin/backend/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const app = express();
-const port = process.env.PORT || 6040;
+const port = process.env.PORT || 5000;
 const moment = require("moment");
 const mongoose = require("mongoose");
 
