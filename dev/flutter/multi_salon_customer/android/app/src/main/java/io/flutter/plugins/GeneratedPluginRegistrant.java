b036c988d31dd38334b69cbf5a5db2a9d9fb51fa
willybrondon <ndanguewilly@gmail.com>
2025-07-26 05:55:55 +0200

    feat_etup_apikey

--- dev/flutter/multi_salon_customer/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ dev/flutter/multi_salon_customer/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -96,6 +96,11 @@ public final class GeneratedPluginRegistrant {
       Log.e(TAG, "Error registering plugin share_plus, dev.fluttercommunity.plus.share.SharePlusPlugin", e);
     }
     try {
+      flutterEngine.getPlugins().add(new io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin());
+    } catch (Exception e) {
+      Log.e(TAG, "Error registering plugin shared_preferences_android, io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin", e);
+    }
+    try {
       flutterEngine.getPlugins().add(new com.csdcorp.speech_to_text.SpeechToTextPlugin());
     } catch (Exception e) {
       Log.e(TAG, "Error registering plugin speech_to_text, com.csdcorp.speech_to_text.SpeechToTextPlugin", e);
--- dev/flutter/multi_salon_customer/macos/Flutter/GeneratedPluginRegistrant.swift
+++ dev/flutter/multi_salon_customer/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -18,6 +18,7 @@ import in_app_purchase_storekit
 import in_app_review
 import path_provider_foundation
 import share_plus
+import shared_preferences_foundation
 import speech_to_text
 import sqflite_darwin
 import url_launcher_macos
@@ -36,6 +37,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   InAppReviewPlugin.register(with: registry.registrar(forPlugin: "InAppReviewPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
