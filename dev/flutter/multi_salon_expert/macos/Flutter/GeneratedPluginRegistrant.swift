589e4fc83a9e0fcdc0c76ec5d2549a788d88c9d9
willybrondon <ndanguewilly@gmail.com>
2025-08-13 10:56:28 +0200

    fix_expert_app_login

--- dev/flutter/multi_salon_expert/macos/Flutter/GeneratedPluginRegistrant.swift
+++ dev/flutter/multi_salon_expert/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,6 +12,7 @@ import flutter_local_notifications
 import in_app_review
 import path_provider_foundation
 import share_plus
+import shared_preferences_foundation
 import sqflite_darwin
 import url_launcher_macos
 
@@ -23,6 +24,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   InAppReviewPlugin.register(with: registry.registrar(forPlugin: "InAppReviewPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
