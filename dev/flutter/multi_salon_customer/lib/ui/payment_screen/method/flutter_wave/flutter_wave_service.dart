936a2cfca0741315af856319a8cd710670999517
willybrondon <ndanguewilly@gmail.com>
2025-11-30 16:33:51 +0100

    fix_flutter_problem

--- dev/flutter/multi_salon_customer/lib/ui/payment_screen/method/flutter_wave/flutter_wave_service.dart
+++ dev/flutter/multi_salon_customer/lib/ui/payment_screen/method/flutter_wave/flutter_wave_service.dart
@@ -112,7 +112,8 @@ class FlutterWaveService {
         date: bookingScreenController.formattedDate.toString(),
         time: bookingScreenController.slotsString.toString(),
         amount: bookingScreenController.totalPrice,
-        withoutTax: bookingScreenController.withOutTaxRupee.toInt(),
+        withoutTax: double.parse(
+            bookingScreenController.withOutTaxRupee.toStringAsFixed(2)),
         paymentType: bookingScreenController.selectedPayment,
         atPlace: bookingScreenController.selectedVenue == "At Salon" ? 1 : 2,
         address: bookingScreenController.searchEditingController.text,
