fa738af20db7d9c2ed8d43b8b073b8080c316b7f
willybrondon <ndanguewilly@gmail.com>
2026-07-01 01:41:19 +0200

    fix_calendar

--- dev/admin/frontend/src/component/tables/calendar/AdminTeamCalendar.js
+++ dev/admin/frontend/src/component/tables/calendar/AdminTeamCalendar.js
@@ -3,9 +3,9 @@ import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { useDispatch, useSelector } from "react-redux";
-import Title from "../extras/Title";
-import { fetchAdminTeamSchedule } from "../../redux/slice/teamScheduleSlice";
-import { getAllSalons } from "../../redux/slice/salonSlice";
+import Title from "../../extras/Title";
+import { fetchAdminTeamSchedule } from "../../../redux/slice/teamScheduleSlice";
+import { getAllSalons } from "../../../redux/slice/salonSlice";
 import { SKEDISY_ADMIN_UI as ui } from "../../../constants/skedisyUiCopy";
 import "./TeamCalendar.css";
 
